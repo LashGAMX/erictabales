ccea156f5a0daa193a92f8f7e5a4c5259dc7f4ca
LashGAMX <alber_199648@hotmail.com>
2024-05-21 17:59:20 -0600

    commit

--- resources/views/home/introduccion.blade.php
+++ resources/views/home/introduccion.blade.php
@@ -44,7 +44,7 @@
             </div>
         </div>
         <div class="swiper-slide slide3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="homeIcon" width="128" height="128" viewBox="0 0 32 32"><path fill="#333333" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z"/></svg>
+            
             <div class="barra">   
             </div>
             <div class="container slide3Scroll">
@@ -94,7 +94,7 @@
             
         </div>
         <div class="swiper-slide">
-            <svg xmlns="http://www.w3.org/2000/svg" class="homeIcon" width="128" height="128" viewBox="0 0 32 32"><path fill="#333333" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z"/></svg>
+            {{-- <svg xmlns="http://www.w3.org/2000/svg" class="homeIcon" width="128" height="128" viewBox="0 0 32 32"><path fill="#333333" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z"/></svg> --}}
             <div class="barra"></div>
             <div class="container slide3Scroll">
                 <div class="row">
@@ -140,7 +140,7 @@
             </div>
         </div>
         <div class="swiper-slide">
-            <svg xmlns="http://www.w3.org/2000/svg" class="homeIcon" width="128" height="128" viewBox="0 0 32 32"><path fill="#333333" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z"/></svg>
+            {{-- <svg xmlns="http://www.w3.org/2000/svg" class="homeIcon" width="128" height="128" viewBox="0 0 32 32"><path fill="#333333" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z"/></svg> --}}
             <div class="barra"></div>
             <div class="container slide3Scroll">
                 <div class="row">
@@ -178,7 +178,7 @@
         
         
         <div class="swiper-slide" id="slideSeccion2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="homeIcon2" width="128" height="128" viewBox="0 0 32 32"><path fill="#333333" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z"/></svg>
+            {{-- <svg xmlns="http://www.w3.org/2000/svg" class="homeIcon" width="128" height="128" viewBox="0 0 32 32"><path fill="#333333" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z"/></svg> --}}
             <div class="barra2 barra">
                 
             </div>
@@ -908,7 +908,7 @@
                 </ul>
             </div>
         </div>
-        
+
         <div class="swiper-slide">
             <div class="barra2 barra">
                 
